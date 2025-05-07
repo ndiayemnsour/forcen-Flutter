88dda9eb46f3045e042a47dff750e77e9a8de5f7
ndiayemnsour <zocod3r@gmail.com>
2025-05-07 13:38:07 +0000

    mise en place d'un popup dynamic

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,17 +1,15 @@
-
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class Event {
   final String confName;
   final Timestamp beginDate;
   final Timestamp signDateEnd;
   final Timestamp endDate;
-  final Int duree;
+  final int duree;
   final String avatar;
   final String status;
-
+//Constructeur
   Event({
     required this.confName,
     required this.beginDate,
@@ -22,17 +20,20 @@ class Event {
     required this.status
 
   });
-
+//convertir un objet en json
   factory Event.fromData(dynamic data){
     return Event(
       confName: data['certifName'],
       beginDate: data['certifBeginDate'],
       signDateEnd: data['signupDate'],
-      endDate: data['endDate'],
+      endDate: data['certifBeginDate'],
       duree: data['duration'],
       avatar: data['avatar'].toString().toLowerCase(),
       status: data['status']
     );
   }
-
+  /// 🔽 Formateurs de date
+  String get formattedBeginDate => DateFormat('dd MMMM yyyy', 'fr_FR').format(beginDate.toDate());
+  String get formattedEndDate => DateFormat('dd MMMM yyyy', 'fr_FR').format(endDate.toDate());
+  String get formattedSignDate => DateFormat('dd MMMM yyyy', 'fr_FR').format(signDateEnd.toDate());
 }
--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:forcen/models/event_model.dart';
 import 'package:intl/intl.dart';
 
 //Page Evenement
@@ -14,31 +15,46 @@ class _EventPageState extends State<EventPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    Future<void> showEventDetails() async {
+    //Alert dialog
+    Future<void> showEventDetails(Event eventData) async {
       return showDialog<void>(
         context: context,
         barrierDismissible: true, // user must tap button!
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text('Force-N'),
+            title:  Text('Certification : ${eventData.confName}',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto_Condensed',
+
+              ),),
+            backgroundColor: Colors.white,
             content: SingleChildScrollView(
               child: ListBody(
                  children: <Widget>[
-                  Image.asset("assets/images/logo.png"),
-                  Text('sa eleeg ci xarale yi'),
-                  Text('Un programme financer par mastercard'),
-                  Text('Formez vous gratuitement'),
+                  Image.asset("assets/images/${eventData.avatar}.png", height: 50,),
+                  Text("Durée de la formation : ${eventData.duree} jours"),
+                  Text("Date Fin d'inscription : ${DateFormat('dd/MM/yyyy').format(eventData.signDateEnd.toDate())}"),
+                  Text("la formation est ${eventData.status}"),
                 ],
               ),
             ),
             actions: <Widget>[
+
               TextButton(
-                child: const Text('Ok'),
+                child: const Text('Fermer'),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
               ),
+
+              TextButton(
+                  onPressed: (){
+
+                  },
+                  child: Text("Postuler")),
+
             ],
           );
         },
@@ -56,23 +72,23 @@ class _EventPageState extends State<EventPage> {
                   return Text("Aucun èvènement trouver dans la base de données");
                 }
 
-                List<dynamic> events = [];
-                snapshot.data!.docs.forEach((element){
-                    events.add(element);
-              });
+                List<Event> events = [];
+                for (var data in snapshot.data!.docs) {
+                    events.add(Event.fromData(data));
+              }
 
                 return ListView.builder( //ListView.builder nous permet de gener dynamiquement les cards avce les contenus
                     itemCount: events.length,
                     itemBuilder: (context, index){
                       final event = events[index];
-                      final avatar = event["avatar"].toString().toLowerCase();
-                      final duration = event["duration"];
-                      final Timestamp timestamp = event["certifBeginDate"];
+                      final avatar = event.avatar;
+                      final duration = event.duree;
+                      final Timestamp timestamp = event.beginDate;
                       final String beginDate = DateFormat('dd/MM/yyyy').format(timestamp.toDate());
-                      final Timestamp timestampSign = event["signupDate"];
+                      final Timestamp timestampSign = event.signDateEnd;
                       final String inscriptionDate = DateFormat('dd/MM/yyyy').format(timestampSign.toDate());
-                      final nameFormation = event['certifName'];
-                      final statut = event['status'];
+                      final nameFormation = event.confName;
+                      final statut = event.status;
 
                       return Card(
                         color: Colors.white,
@@ -82,7 +98,7 @@ class _EventPageState extends State<EventPage> {
                           title: Text('$nameFormation ($statut - $duration jours)  '),
                           subtitle: Text("Fin d'inscription : $inscriptionDate \nDébut de formation : $beginDate"),
                           trailing: IconButton(
-                              onPressed: (){showEventDetails();},
+                              onPressed: (){showEventDetails(event);},
                               icon: Icon(Icons.info, color: Colors.blue)
                           ),
 
